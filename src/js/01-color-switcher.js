56293f90c025600a0c1dae546461e338b3e87331
Andrii Larin <alarin558@gmail.com>
2023-05-15 21:57:28 +0300

    is working

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
+let intervalId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+stopBtn.disabled = true;
+
+function onStartBtnClick (event) {
+  event.target.disabled = true;
+  stopBtn.disabled = false;
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onStopBtnClick (event) {
+  clearInterval(intervalId);
+  event.target.disabled = true;
+  startBtn.disabled = false;
+}
